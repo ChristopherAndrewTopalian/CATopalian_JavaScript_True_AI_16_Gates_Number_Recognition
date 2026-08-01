f95997dfe6386d05b88dbb12785c381c525c3fde
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-01 08:09:15 -0400

    update

--- src/js/sound/sound.js
+++ src/js/sound/sound.js
@@ -17,19 +17,29 @@ function playTone(frequency, duration, waveType, volume, detune, attackTime, rel
     gainNode.connect(pannerNode);
     pannerNode.connect(audioCtx.destination);
 
+    // Convert milliseconds to seconds for the audio context
+    let durationInSeconds = duration / 1000;
+
+    // THE ENVELOPE (Attack, Sustain, Release)
+
+    // Initialize volume at 0
     gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
 
-    // attack
+    // Attack: Ramp up to the target volume
     gainNode.gain.linearRampToValueAtTime(volume, audioCtx.currentTime + attackTime);
-    gainNode.gain.linearRampToValueAtTime(0, 
 
-    // release
-    audioCtx.currentTime + duration / 1000 - releaseTime);
+    // Sustain: Hold the target volume until it is time to release
+    gainNode.gain.setValueAtTime(volume, audioCtx.currentTime + durationInSeconds - releaseTime);
 
+    // Release: Ramp the volume back down to 0
+    gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + durationInSeconds);
+
+    // Set stereo panning
     pannerNode.pan.setValueAtTime(panValue, audioCtx.currentTime);
 
-    oscillator.start();
-    oscillator.stop(audioCtx.currentTime + duration / 1000);
+    // Start and stop the oscillator
+    oscillator.start(audioCtx.currentTime);
+    oscillator.stop(audioCtx.currentTime + durationInSeconds);
 }
 
 function hoverSound()
@@ -60,27 +70,13 @@ function hoverSound2()
     );
 }
 
-function hoverSound2()
-{
-    playTone(
-        300,     // frequency
-        300,     // duration
-        'triangle',  // waveType
-        0.08,    // volume
-        -200,    // detune
-        0.1,      // attackTime
-        0.1,      // releaseTime
-        0.0       // panValue -0.5 is left
-    );
-}
-
 function clickSound()
 {
     playTone(
-        400,     // frequency
-        500,     // duration
+        200,     // frequency
+        100,     // duration
         'sine',  // waveType
-        0.04,      // volume
+        0.02,      // volume
         0,       // detune
         0.1,      // attackTime
         0.1,      // releaseTime
